46026e880911ed62dcd93b7720572bfa4f5d7c24
Steven Shatz <sshatzdeveloper@gmail.com>
2024-05-11 15:56:55 -0400

    Add links, specify language, max 20 results, drop removed items, prompt for topic

--- main.py
+++ main.py
@@ -14,7 +14,26 @@ from send_email_2 import send_email_2
 
 api_key = "596005ba759644559a58f1f80a18c0c4"
 
-url = "https://newsapi.org/v2/everything?q=tesla&from=2024-04-10&sortBy=publishedAt&apiKey=" + api_key
+print("News Finder:")
+
+topic = ""
+while not topic:
+    topic = input("What are you searching for? ").strip().lower()
+
+topic = '"' + topic + '"'  # surround phrase with quotes (eg: "star wars")
+print("Processing...")
+
+# Search all elements of dictionary for "Tesla" (&everything?q=tesla)
+# Select articles published 4/10/24 and later (&from=2024-04-10)
+# Select English only (&language=en)
+# Select a max of 50 articles (&pageSize=50)
+# Sort by publication date (&sortBy=publishedAt)
+url = f"https://newsapi.org/v2/everything?q={topic}" \
+      "&from=2024-04-10" \
+      "&language=en" \
+      "&pageSize=50" \
+      "&sortBy=publishedAt" \
+      f"&apiKey={api_key}"
 
 # Make a request
 r = requests.get(url)
@@ -30,28 +49,33 @@ content = r.json()  # Make data more accessible:
 
 # Access the article titles and descriptions within the dictionary
 message = ""
-for article in content["articles"]:
-    # print(article["title"])
-    # print(article["description"])
-
-    title = article["title"]
-    description = article["description"]
+for article in content["articles"][:20]:  # Only process 1st 20 articles
+    title = article["title"].strip()
+    description = article["description"].strip()
+    url = article["url"]
 
     # Handle situations in which title and/or description are 'None'
-    if title:
+    if title == "[Removed]":  # Drop articles that are flagged as "[Removed]"
+        continue
+    elif title:
         message += title + '\n'
     else:
         message += "[No Title]" + '\n'
 
     if description:
-        message += description + (2 * '\n')
+        message += description + '\n'
     else:
-        message += "[No Description]" + (2 * '\n')
+        message += "[No Description]" + '\n'
+
+    if url:
+        message += url + (2 * '\n')
+    else:
+        message += "[No Link]" + (2 * '\n')
 
 # Method 1:
 # ---------
-# message = """\
-# Subject: Latest Tesla News
+# message = f"""\
+# Subject: Latest {topic.strip('"').title()} News
 #
 # """ + message                      # Insert Subject at start of message
 # message = message.encode("utf-8")  # Convert to UTF-8 encoding
@@ -59,4 +83,5 @@ for article in content["articles"]:
 
 # Method 2:
 # ---------
-send_email_2(subject="Latest Tesla News", message=message)
+send_email_2(subject=f"Latest {topic.title()} News", message=message)
+print("Your email was sent successfully.")
